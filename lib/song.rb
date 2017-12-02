1f175476b4087c46b9e9a6a31cffa484a4057e0d
Matthew Hunger <matt.hunger@gmail.com>
2017-12-02 20:41:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     count_genre = {}
     @@genres.each do |r|
       if count_genre[r] == nil
-        count_genre[r] = 0
+        count_genre[r] = 1
       else
         count_genre[r] += 1
       end
@@ -46,7 +46,7 @@ class Song
     count_artist = {}
     @@artists.each do |t|
       if count_artist[t] == nil
-        count_artist[t] = 0
+        count_artist[t] = 1
       else
         count_artist[t] += 1 #[t] += 1
       end
